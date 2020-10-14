27df0211fb525891fc52a391fd8a83f3d61a56d1
aliceniu <72165478+aliceniu@users.noreply.github.com>
2020-10-14 03:51:37 -0400

    Add files via upload

---
+++ week2-hw.py
@@ -0,0 +1,40 @@
+# Assignment 1: daily object
+# object 1
+myTeapot = {
+    "dimension": "9in x 6in",
+    "color": ["gold", "white"],
+    "brand": "SELETTI",
+    "material": "fine porcelain",
+    "designer": "Selab",
+    "numberOfTeaCup": 1
+}
+
+print myTeapot
+print myTeapot["material"]
+
+
+# object 2
+myToothbrush = {
+    "brand": "Phlip",
+    "type": "Electronic toothbrush",
+    "color": "pink",
+    "quantity": 1
+}
+
+print myToothbrush
+print myToothbrush["type"]
+
+
+
+# object 3
+myPencilcase = ["mechanipencil", "pen", "eraser", "bone creaser", "highlighter"]
+
+print myPencilcase[0]
+
+# object 4
+myPen = {
+    "color": "transparent",
+    "brand": "PILOT",
+    "inkColor": "black",
+    "broken": False
+}
